7d8e5e3b9e3fcca9e9434c32bbcf6b35908dade7
Jaewon Lee <amshoo82@gmail.com>
2023-09-04 17:34:23 -0400

    remove print statements

--- assn1_torch.py
+++ assn1_torch.py
@@ -90,9 +90,9 @@ class MyLinear(nn.Module):
         self.w1 = nn.Parameter(torch.nn.init.normal_(torch.randn(input_dim, output_dim), mean=mean, std=std_dev))
         self.w2 = nn.Parameter(torch.nn.init.normal_(torch.randn(input_dim, output_dim), mean=mean, std=std_dev))
         self.b = nn.Parameter(torch.ones(output_dim))
-        print(input_dim, output_dim)
-        print(self.w1, self.w2, self.b)
-        print(self.w1.shape, self.w2.shape, self.b.shape)
+        # print(input_dim, output_dim)
+        # print(self.w1, self.w2, self.b)
+        # print(self.w1.shape, self.w2.shape, self.b.shape)
 
         ############## END CODE HERE
 
@@ -110,7 +110,7 @@ class MyLinear(nn.Module):
         #    output = XW_1 + X^2W_2 + b \\
         outputs = torch.matmul(inputs, self.w1)+torch.matmul(inputs**2, self.w2)
         outputs = torch.add(outputs,self.b)
-        print(outputs.shape, self.b.shape)
+        #print(outputs.shape, self.b.shape)
         
         ############## END CODE HERE
 
