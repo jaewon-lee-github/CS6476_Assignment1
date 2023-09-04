ffbdb76959443e0400b682c7b8e5e91f94b1aff0
Jaewon Lee <amshoo82@gmail.com>
2023-09-04 17:25:58 -0400

    mod

--- assn1_torch.py
+++ assn1_torch.py
@@ -90,6 +90,7 @@ class MyLinear(nn.Module):
         self.w1 = nn.Parameter(torch.nn.init.normal_(torch.randn(input_dim, output_dim), mean=mean, std=std_dev))
         self.w2 = nn.Parameter(torch.nn.init.normal_(torch.randn(input_dim, output_dim), mean=mean, std=std_dev))
         self.b = nn.Parameter(torch.ones(input_dim, output_dim))
+        print(input_dim, output_dim)
         print(self.w1, self.w2, self.b)
         print(self.w1.shape, self.w2.shape, self.b.shape)
 
